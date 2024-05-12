6ed9df1308ed4bcda1d8a7903b7a75afed76e8ee
jambhaleAnuj <jambhaleanuj@gmail.com>
2024-05-12 20:36:52 +0530

    Initial commit

---
+++ src/data/info.ts
@@ -0,0 +1,85 @@
+export const info = {
+  name: "Anuj Jambhale",
+  brief_description:
+    "I'm a full stack developer with a knack for Game Development.",
+  role: "Full Stack Developer",
+  picture: "/my_photo.png",
+  picture_alt: "Bart Simpson",
+  location: "Mumbai, India",
+  cv: "/My_Resume.pdf",
+
+  about: {
+    description: `I'm a full stack developer with a passion for causing a little chaos in web development.
+    I have a solid foundation in computer science, although I prefer skateboarding over studying.
+    I excel in building web applications using React, Node.js, and MongoDB.
+    I'm known for my sharp wit and quick thinking, traits that serve me well in coding and pranks alike.
+    Looking for a challenge? Let's create something memorable together.`,
+    education: [
+      {
+        title: "Bachelors in Science, Computer Science ",
+        date: "2020 - 2023",
+        location: "Tilak College of Science and Commerce, Vashi",
+        gpa: "9.68 CGPA",
+        
+      },
+      {
+        title: "Masters in Science, Computer Science ",
+        date: "2023 - Present",
+        location: "SIES College of Arts, Science and Commerce, Nerul",
+        
+        
+      },
+    ],
+    experience: [
+      {
+        title: "Web Developer Intern",
+        date: "Present",
+        location: "Rasheed Foundation, Navi Mumbai",
+        description:
+          "Crafted interactive web experiences using PHP.",
+      },
+    ],
+
+    skills: ["JavaScript", "React", "Node.js", "MongoDB", "C#", "PHP"], // not used yet
+  },
+
+  projects: [
+    {
+      title: "Java Point of Sales with Android Barcode Scanner Application",
+      date: "2022",
+      description:
+        "A POS system made with Java for desktop which utilizes the Android Barcode Scanner Application instead of the Physical barcode scanner",
+      link: "https://github.com/jambhaleAnuj/java_point_of_sales_3",
+      tech: ["Java", "IReport", "Zxing", "Android" ],
+      img_alt: "Anuj's Portfolio",
+      img_path: "/pos.png",
+    },
+    {
+      title: "Music control using hand gesture recognition",
+      date: "2023",
+      description:
+        "this project demonstrates how to combine computer vision, machine learning, and audio functionalities to create an interactive music control system based on hand gestures.",
+      link: "https://github.com/jambhaleAnuj/Music-control-with-hand-gesture-recognition",
+      tech: ["Python", "Tensorflow", "OpenCV", "pygame"],
+      img_alt: "Music control using hand gesture recogntion",
+      img_path: "hand_recognition.png",
+    },
+    {
+      title: "Ping Pong Game made using Unity game engine",
+      date: "2022",
+      description:
+        "This ping pong game is made in Unity game engine. This game has 2 modes: Single Player and Multiplayer",
+      link: "https://github.com/bartsimpsonfake/comic-club",
+      tech: ["Unity Engine", "C#"],
+      img_alt: "Ping pong ",
+      img_path: "ping_pong.png",
+    },
+  ],
+
+  contact: {
+    email: "jambhaleanuj@gmail.com",
+    linkedin: "https://www.linkedin.com/in/anuj-jambhale-856689245/",
+    github: "https://www.github.com/jambhaleAnuj",
+    twitter: "https://twitter.com/anujjambhale",
+  },
+};
