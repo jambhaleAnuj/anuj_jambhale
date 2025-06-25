68fce86e9d53148e247dc1aca943ce60f66a2e3c
anujjambhale <jambhaleanuj@gmail.com>
2025-06-25 19:40:23 +0530

    Update configuration and enhance SEO with Open Graph and Twitter meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import robotsTxt from "astro-robots-txt";
-import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
+import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
 
 import sitemap from "@astrojs/sitemap";
 
@@ -11,6 +11,6 @@ export default defineConfig({
   markdown: {
     rehypePlugins: [rehypeAccessibleEmojis],
   },
-  site: 'https://astro-portfolio-template.pages.dev',
-  integrations: [tailwind(), react(), robotsTxt(), sitemap()]
+  site: "htttps://www.jambhaleanuj.tech",
+  integrations: [tailwind(), react(), robotsTxt(), sitemap()],
 });
