392f7d54d66d0fd1423344a127e3e68e1d7cd814
anujjambhale <jambhaleanuj@gmail.com>
2025-06-25 19:42:36 +0530

    Refine project descriptions and update image paths in info.ts

--- src/data/info.ts
+++ src/data/info.ts
@@ -44,37 +44,39 @@ export const info = {
       title: "Java Point of Sales with Android Barcode Scanner Application",
       date: "2022",
       description:
-        "A POS system made with Java for desktop which utilizes the Android Barcode Scanner Application instead of the Physical barcode scanner",
+        "A POS system made with Java for desktop which utilizes the Android Barcode Scanner Application instead of the Physical barcode scanner.",
       link: "https://github.com/jambhaleAnuj/java_point_of_sales_3",
       tech: ["Java", "IReport", "Zxing", "Android"],
-      img_alt: "Anuj's Portfolio",
+      img_alt:
+        "Screenshot of Java Point of Sales with Android Barcode Scanner Application interface.",
       img_path: "/pos.png",
     },
     {
       title: "Music control using hand gesture recognition",
       date: "2023",
       description:
-        "this project demonstrates how to combine computer vision, machine learning, and audio functionalities to create an interactive music control system based on hand gestures.",
+        "This project demonstrates how to combine computer vision, machine learning, and audio functionalities to create an interactive music control system based on hand gestures.",
       link: "https://github.com/jambhaleAnuj/Music-control-with-hand-gesture-recognition",
       tech: ["Python", "Tensorflow", "OpenCV", "pygame"],
-      img_alt: "Music control using hand gesture recogntion",
-      img_path: "hand_recognition.png",
+      img_alt:
+        "Screenshot of music control system using hand gesture recognition.",
+      img_path: "/hand_recognition.png",
     },
     {
       title: "Ping Pong Game made using Unity game engine",
       date: "2022",
       description:
-        "This ping pong game is made in Unity game engine. This game has 2 modes: Single Player and Multiplayer",
+        "A ping pong game made in Unity with Single Player and Multiplayer modes.",
       link: "https://github.com/bartsimpsonfake/comic-club",
       tech: ["Unity Engine", "C#"],
-      img_alt: "Ping pong ",
-      img_path: "ping_pong.png",
+      img_alt: "Screenshot of Ping Pong Game made in Unity.",
+      img_path: "/ping_pong.png",
     },
   ],
 
   contact: {
     email: "jambhaleanuj@gmail.com",
-    linkedin: "https://www.linkedin.com/in/anuj-jambhale-856689245/",
+    linkedin: "https://www.linkedin.com/in/anuj-jambhale/",
     github: "https://www.github.com/jambhaleAnuj",
     twitter: "https://twitter.com/anujjambhale",
   },
